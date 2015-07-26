7e9b360fc1ad4594b7265156e60b104d20aae216
George231086 <george231086@yahoo.co.uk>
2015-07-26 18:22:52 +0100

    Create AnagramMaker.cs

---
+++ AnagramGame/AnagramMaker.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AnagramGame
+{
+    class AnagramMaker
+    {
+
+        public static void modHeapsAlgo(int n, char[] word, ref int permNo, ref string anagram)
+        {
+            if (n == 1)
+            {
+                permNo--;
+                if (permNo == 0)
+                {
+                    string wordStr = "";
+                    foreach (char c in word)
+                    {
+                        wordStr += c;
+                    }
+                    anagram = wordStr;
+                    return;
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    modHeapsAlgo(n - 1, word, ref permNo, ref anagram);
+                    if (n % 2 == 0)
+                    {
+                        char temp = word[n - 1];
+                        word[n - 1] = word[i];
+                        word[i] = temp;
+                    }
+                    else
+                    {
+                        char temp = word[n - 1];
+                        word[n - 1] = word[0];
+                        word[0] = temp;
+                    }
+                }
+
+                modHeapsAlgo(n - 1, word, ref permNo, ref anagram);
+            }
+
+        }
+
+        public static int factorial(int i)
+        {
+            if (i <= 1)
+                return 1;
+
+            return i * factorial(i - 1);
+        }
+
+
+        public string[] getAnagram(string s)
+        {
+            int wordLength = s.Length;
+            int i1 ;
+            if (wordLength <= 6)
+                i1 = factorial(wordLength)/4;
+            else
+                i1 = factorial(6)/4;
+            int i2 = 3*i1;
+            
+            Random r = new Random();
+            int permNo = 0;
+            permNo = r.Next(i1, i2);
+            string anagram = "";
+            modHeapsAlgo(wordLength, s.ToCharArray(), ref permNo, ref anagram);
+            return new string[] { s, anagram };
+        }
+
+    }
+}
